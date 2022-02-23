047881e0f738480739115798d075e7283dd2fdf9
Sahul Kumar Parida <71061738+sahuljr7@users.noreply.github.com>
2022-02-23 20:34:21 +0530

    Create squareroot.cpp

---
+++ squareroot.cpp
@@ -0,0 +1,32 @@
+// https://practice.geeksforgeeks.org/problems/square-root/1#
+
+// { Driver Code Starts
+//Initial Template for C
+
+#include<stdio.h>
+  
+
+ // } Driver Code Ends
+//User function Template for C
+
+long long int floorSqrt(long long int x) 
+{
+    return floor(sqrt(x));    
+}
+
+// { Driver Code Starts.
+
+int main()
+{
+	int t;
+	scanf("%d", &t);
+	while(t--)
+	{
+		long long n;
+		scanf("%ld", &n);
+	
+		printf("%ld\n", floorSqrt(n));
+	}
+    return 0;   
+}
+  // } Driver Code Ends
